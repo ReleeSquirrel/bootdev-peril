f47baab17acd579b8d6c7facf31400e89542ae6d
ReleeSquirrel <relee_s@hotmail.com>
2026-02-16 12:50:08 -0500

    Set up persistance in the server's main function

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,20 @@
+import amqp from "amqplib";
+
 async function main() {
   console.log("Starting Peril server...");
+  const connectionString = `amqp://guest:guest@localhost:5672/`;
+  const connection = await amqp.connect(connectionString);
+  console.log(`Connection was successful!`);
+  process.on('SIGINT', () => {
+    console.log(`Program is shutting down.`);
+    connection.close();
+    process.exit(0);
+  });
+  process.stdin.resume();
+}
+
+function endProgram() {
+  process.exit(0);
 }
 
 main().catch((err) => {
